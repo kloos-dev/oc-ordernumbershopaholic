d40e986a79ee4eb7dc3761f4766a56499bf77acd
Sebastiaan Kloos <sebastiaan@codecycler.com>
2020-07-31 10:47:55 +0200

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,39 @@
+<?php namespace Codecycler\OrdernumberShopaholic;
+
+use Codecycler\OrdernumberShopaholic\Classes\Helper\OrderNumberFormat;
+use Lovata\OrdersShopaholic\Models\Order;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+        return [
+            'settings' => [
+                'label'       => 'Additional settings',
+                'description' => 'Manage additional settings',
+                'category'    => 'Webshop',
+                'icon'        => 'icon-star',
+                'class'       => 'Codecycler\OrdernumberShopaholic\Models\Settings',
+                'order'       => 500,
+                'keywords'    => 'order number format orders',
+                'permissions' => []
+            ]
+        ];
+    }
+
+    public function boot()
+    {
+        Order::extend(function ($obModel) {
+            $obModel->bindEvent('model.beforeSetAttribute', function ($sKey, $sValue) use ($obModel) {
+                if ($sKey == 'order_number') {
+                    return OrderNumberFormat::instance()->format($obModel);
+                }
+            });
+        });
+    }
+}
---
+++ classes/helper/OrderNumberFormat.php
@@ -0,0 +1,61 @@
+<?php namespace Codecycler\OrdernumberShopaholic\Classes\Helper;
+
+use Carbon\Carbon;
+use Lovata\OrdersShopaholic\Models\Order;
+use October\Rain\Support\Traits\Singleton;
+use Codecycler\OrdernumberShopaholic\Models\Settings;
+
+class OrderNumberFormat
+{
+    use Singleton;
+
+    public $obOrder;
+
+    public $sFormat;
+
+    public $obLastOrder;
+
+    public function format($obOrder)
+    {
+        if (!Settings::get('order_number_custom', false)) {
+            return $obOrder->order_number;
+        }
+
+        $this->obOrder = $obOrder;
+        $this->sFormat = Settings::get('order_number_format', '@y-@n(6)');
+
+        $this->obLastOrder = Order::orderBy('id', 'desc')->get();
+
+        $this->iOrderNumberCount = Settings::get('order_number_count', 1);
+
+        $this->replaceYear();
+        $this->replaceNumber();
+
+        // Update the order count index
+        Settings::set('order_number_count', $this->iOrderNumberCount + 1);
+
+        return $this->sFormat;
+    }
+
+    public function replaceNumber()
+    {
+        $match = preg_match('/(@n)(\(\d\))/i', $this->sFormat, $matches);
+
+        if ($match) {
+            $iNumberInt = preg_replace('/[\(\)]+/', '', $matches[2]);
+
+            // Generate new number
+            $sOrderCount = (string) $this->iOrderNumberCount;
+            $sNumber = str_pad($sOrderCount, $iNumberInt, "0", STR_PAD_LEFT);
+
+            $this->sFormat = preg_replace('/@n\([\d]\)/', $sNumber, $this->sFormat);
+        }
+    }
+
+    public function replaceYear()
+    {
+        $thisYear = Carbon::now()->format('Y');
+
+        $this->sFormat = str_replace('@y', $thisYear, $this->sFormat);
+    }
+}
