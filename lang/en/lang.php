ea868e6016191c38d0b7b00b41c5251f7c4d623e
Роман Саркиев <sarkiev@gmail.com>
2020-08-03 09:06:13 +0300

    Added Russian lang

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,22 @@
 <?php return [
     'plugin' => [
-        'name' => 'OrdernumberShopaholic',
-        'description' => ''
-    ]
+        'name'        => 'Order Number Shopaholic',
+        'description' => 'Changing order numbering rules'
+    ],
+    'settigns' => [
+        'label'       => 'Additional settings',
+        'description' => 'Manage additional settings',
+        'category'    => 'Webshop',
+    ],
+    'tab' => [
+        'order_number' => 'Order number'
+    ],
+    'field' => [
+        'order_number_custom' => 'Custom order number format',
+        'order_number_format' => 'Order number format',
+        'order_number_count' => 'Order number count',
+    ],
+    'comment' => [
+        'order_number_count' => 'Only change this value if you know what you are doing ;-)',
+    ],
 ];
